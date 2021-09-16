3020621b67d2c53cec0f92d0c19053fcf89a6e75
codinginpain <51408335+codinginpain@users.noreply.github.com>
2021-09-16 12:39:06 +0900

    made pages

---
+++ lib/components/recipe_list_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RecipeListItem extends StatelessWidget {
+  final String imageName;
+  final String title;
+
+  const RecipeListItem(this.imageName, this.title);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
