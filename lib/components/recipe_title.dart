de3245d19210fb2a3fc7184f5a84ac321ea19489
codinginpain <51408335+codinginpain@users.noreply.github.com>
2021-09-18 18:14:22 +0900

    title and font set

--- lib/components/recipe_title.dart
+++ lib/components/recipe_title.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class RecipeTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Text(
+        "Recipes",
+        style: TextStyle(fontSize: 30),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: "PatuaOne"),
       home: RecipePage(),
     );
   }
